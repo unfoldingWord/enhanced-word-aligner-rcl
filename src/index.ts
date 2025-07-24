a9deeae2ce62295ec329cc58e58b0f8ea11cfe1f
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2025-07-24 11:39:53 -0400

    added index

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+// Export components
+export { WordAlignerComponent } from './components/WordAlignerComponent';
+
+// If you have types that should be exposed to consumers, export them as well
+// For example (you would need to replace these with your actual types):
+// export type { HelloCardProps } from './HelloCard';
+// export type { WordAlignerComponentProps } from './components/WordAlignerComponent';
+
+// You can also re-export from other index files if you have them
+// For example:
+// export * from './common';
