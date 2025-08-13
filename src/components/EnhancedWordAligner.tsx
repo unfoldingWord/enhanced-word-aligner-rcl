aca2290de0caa60d70946cbdbdbbe2854db39d73
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2025-08-13 09:25:36 -0400

    removed HelloCard component, associated styles, and documentation. Updated `EnhancedWordAligner` and exports to eliminate unused references.

--- src/components/EnhancedWordAligner.tsx
+++ src/components/EnhancedWordAligner.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useRef, useState} from 'react'
-import '../App.css'
 import {SuggestingWordAligner} from 'word-aligner-rcl'
 import {
     ContextId,
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 
 // Export components
-export { EnhancedWordAligner } from './components/EnhancedWordAligner';
-export {
+import { EnhancedWordAligner } from './components/EnhancedWordAligner';
+import {
     AlignmentHelpers,
     bibleHelpers,
     SuggestingWordAligner,
     UsfmFileConversionHelpers,
     usfmHelpers
 } from 'word-aligner-rcl'
+
+export {
+    AlignmentHelpers,
+    bibleHelpers,
+    EnhancedWordAligner,
+    SuggestingWordAligner,
+    UsfmFileConversionHelpers,
+    usfmHelpers
+} 
